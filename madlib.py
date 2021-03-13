6ca8be74e8cfa294a9f5d1283528a19d4ee37744
krishnaasrinivas <67292053+krishnaasrinivas@users.noreply.github.com>
2021-03-14 01:54:27 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,10 @@
+#madlibs is string concatenation
+#fstring is used
+
+name = input("name : ") 
+age =  input ("age : ")
+adj = input ("adjective : ")
+madlib = f"Hello, My name is {name}, i am {age} years old.\n I find python to be super {adj}!!"
+
+
+print(madlib)
